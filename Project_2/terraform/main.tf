8f3679dd61cc619a32a43603641c99a772dadaac
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-26 09:47:54 +0600

    Update main.tf

--- Project_2/terraform/main.tf
+++ Project_2/terraform/main.tf
@@ -1,7 +1,23 @@
-variable "SUBSCRIPTION_ID" {}
-variable "CLIENT_ID" {}
-variable "CLIENT_SECRET" {}
-variable "TENANT_ID" {}
+variable "SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "CLIENT_ID" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "CLIENT_SECRET" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "TENANT_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+}
 
 provider "azurerm" {
   features {}
