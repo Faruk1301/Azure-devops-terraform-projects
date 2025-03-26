8dddbccda412b073b1cc5e158e3fc512570986d4
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-03-26 09:42:27 +0600

    Update main.tf

--- Project_2/terraform/main.tf
+++ Project_2/terraform/main.tf
@@ -1,5 +1,14 @@
+variable "SUBSCRIPTION_ID" {}
+variable "CLIENT_ID" {}
+variable "CLIENT_SECRET" {}
+variable "TENANT_ID" {}
+
 provider "azurerm" {
   features {}
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.CLIENT_ID
+  client_secret   = var.CLIENT_SECRET
+  tenant_id       = var.TENANT_ID
 }
 
 # Resource Group
