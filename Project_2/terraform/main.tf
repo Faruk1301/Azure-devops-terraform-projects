a6b7d6c5cf86d396e1c25e029200472c864cdbb2
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-08 00:24:42 +0600

    Update main.tf

--- Project_2/terraform/main.tf
+++ Project_2/terraform/main.tf
@@ -2,15 +2,14 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "dev-vnet-rg"
-  location = "East US"
+resource "azurerm_resource_group" "vnet_rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "dev-vnet"
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.vnet_rg.location
+  resource_group_name = azurerm_resource_group.vnet_rg.name
 }
-
