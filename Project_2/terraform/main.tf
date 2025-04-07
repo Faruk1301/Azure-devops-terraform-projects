a609ddb2d513121be6c007403b095bed1a917066
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-08 00:52:58 +0600

    Update main.tf

--- Project_2/terraform/main.tf
+++ Project_2/terraform/main.tf
@@ -1,6 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "vnet_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 resource "azurerm_virtual_network" "main" {
   name                = var.vnet_name
-  address_space       = var.address_space  # Now uses the variable
+  address_space       = ["10.0.0.0/16"] # Hardcoded to simplify
   location            = azurerm_resource_group.vnet_rg.location
   resource_group_name = azurerm_resource_group.vnet_rg.name
 }
