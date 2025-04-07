613bbb5db1a891278f62ef665d0f86cc6333a3f2
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-08 00:53:30 +0600

    Update variables.tf

--- Project_2/terraform/variables.tf
+++ Project_2/terraform/variables.tf
@@ -1,21 +1,15 @@
 variable "resource_group_name" {
-  description = "Name of the Azure Resource Group"
   type        = string
+  description = "Name of the resource group"
 }
 
 variable "vnet_name" {
-  description = "Name of the Virtual Network"
   type        = string
+  description = "Name of the virtual network"
 }
 
 variable "location" {
-  description = "Azure region for resources"
   type        = string
   default     = "eastus"
-}
-
-variable "address_space" {
-  description = "The address space for the virtual network"
-  type        = list(string)
-  default     = ["10.0.0.0/16"]  # Added default value
+  description = "Azure region for resources"
 }
