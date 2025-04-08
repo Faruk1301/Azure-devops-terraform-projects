ea6ed4cd9bd0a3373a1925ffa45f356b40772d46
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-08 10:51:01 +0600

    Update main.tf

--- Project_2/terraform/main.tf
+++ Project_2/terraform/main.tf
@@ -1,15 +1,37 @@
 terraform {
+  required_version = "~> 1.6.6"
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.85.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
   skip_provider_registration = true
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
 }
 
 resource "azurerm_resource_group" "vnet_rg" {
@@ -19,7 +41,22 @@ resource "azurerm_resource_group" "vnet_rg" {
 
 resource "azurerm_virtual_network" "main" {
   name                = var.vnet_name
-  address_space       = ["10.0.0.0/16"] # Hardcoded to simplify
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.vnet_rg.location
   resource_group_name = azurerm_resource_group.vnet_rg.name
+  
+  tags = {
+    Environment = "Production"
+    Terraform   = "true"
+  }
+}
+
+output "vnet_id" {
+  description = "Virtual Network ID"
+  value       = azurerm_virtual_network.main.id
+}
+
+output "vnet_name" {
+  description = "Virtual Network Name"
+  value       = azurerm_virtual_network.main.name
 }
