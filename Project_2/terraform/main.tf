6f7ae48ab11c66b30e3b4194e4093b82551159f6
Md.Faruk Hossain <faruk.cse.pust12@gmail.com>
2025-04-08 10:56:47 +0600

    Update main.tf

--- Project_2/terraform/main.tf
+++ Project_2/terraform/main.tf
@@ -1,15 +1,25 @@
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the resource group"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
-variable "vnet_name" {
-  type        = string
-  description = "Name of the virtual network"
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
 }
 
-variable "location" {
-  type        = string
-  default     = "eastus"
-  description = "Azure region for resources"
+resource "azurerm_resource_group" "vnet_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.vnet_rg.location
+  resource_group_name = azurerm_resource_group.vnet_rg.name
 }
